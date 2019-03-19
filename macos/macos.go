824edc88ce85ecd95bf9c1320a33f13347605c65
C3NZ <cenz@cenz.io>
2019-03-18 18:45:19 -0700

    [fix] refactor Installers to use the commander function

--- macos/macos.go
+++ macos/macos.go
@@ -2,34 +2,29 @@ package macos
 
 // InstallXCode checks if xcode has been installed, which is needed for 
 // the user to install any other dependency
-func InstallXCode(commander func(string)) {
+func InstallXCode(commander func(string, int)) {
     // Xcode download link
-   // xCodeInstall := "https://itunes.apple.com/us/app/xcode/id497799835?mt=12"
+    xCodeInstall := "https://itunes.apple.com/us/app/xcode/id497799835?mt=12"
 
-    commander("found=false\n")
-//    // setup found flag, for if xcode is found
-//    genericScript = append(genericScript, "found=false\n")
-//    
-//    // Check if xcode has been installed, then install command line tools if so 
-//    genericScript = append(genericScript, "if command -v xcode-select &> /dev/null; then")
-//    genericScript = append(genericScript, "\t" + "found=true")
-//    genericScript = append(genericScript, "\t" + "echo \"Xcode installed, installing commandline tools if not already installed\"")
-//    genericScript = append(genericScript, "\t" + "xcode-select --install 2> /dev/null") 
-//    genericScript = append(genericScript, "else")
-//    genericScript = append(genericScript, "\t" + "printf \"Xcode is not installed, would you like to install it now? (y/n): \"")
-//    genericScript = append(genericScript, "\t" + "read installXCode")
-//    
-//    // Check if the user had entered yes for installing xcode, if so link them to xcode
-//    genericScript = append(genericScript, "\t" + "if [ $installXCode = \"y\"] || [ $installXCode = \"yes\" ]; then")
-//    genericScript = append(genericScript, "\t\t" + "echo \"Opening up the browser to the app stores Xcode page\"")
-//    genericScript = append(genericScript, "\t\t" + "open " + xCodeInstall)
-//    genericScript = append(genericScript, "\t\t" + "echo \"After installing XCode, please rerun the script and xcode tools shall be installed upon next run\"")
-//    genericScript = append(genericScript, "\t" + "else")
-//    genericScript = append(genericScript, "\t\t" + "echo \" Xcode is needed for anything else to install. Shutting down.\"")
-//    genericScript = append(genericScript, "\t" + "exit")
-//    genericScript = append(genericScript, "\t" + "fi") 
-//    genericScript = append(genericScript, "fi")
+    // Check if the user has xcode installed and attempt to install the xcode CLI tools
+    // if they're not already installed
+    commander("if command -v xcode-select &> /dev/null; then", 0)
+    commander("echo \"Xcode installed, installing commandline tools if not already installed\"", 1)
+    commander("xcode-select --install 2> /dev/null", 1)
 
+    // Handle if its 
+    commander("else", 0)
+    commander("echo \"Xcode is not installed, would you like to install it now? (y/n)\"", 1)
+    commander("read installXCode", 1)
+    commander("if [ $installXCode = \"y\"] || [ $installXCode = \"yes\" ]; then", 1)
+    commander("echo \"Opening up the browser to the app stores Xcode page\"", 2)
+    commander("open " + xCodeInstall, 2)
+    commander("echo \"After installing XCode, please rerun the script and xcode tools shall be installed upon next run\"", 2)
+    commander("else", 1)
+    commander("echo \" Xcode is needed for anything else to install. Shutting down.\"", 2)
+    commander("exit", 1)
+    commander("fi", 1)
+    commander("fi", 0)
 }
 
 func InstallBrew(genericScript []string) []string {
