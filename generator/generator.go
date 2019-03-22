09e8ba7c7c8f1b9d23a6fa4dc18777ab4e39835a
C3NZ <cenz@cenz.io>
2019-03-21 21:24:18 -0700

    [feat & fix] support a new category type, refactor the install request, and create a supported mac packages variable for sending to the frontend

--- generator/generator.go
+++ generator/generator.go
@@ -8,22 +8,85 @@ import (
 
     "github.com/quicky-dev/generator/macos"
     "github.com/google/uuid"
+    "github.com/mitchellh/mapstructure"
 )
 
 var filePath string;
 var debug bool;
 
+type category struct{
+    Description string
+    Items []string
+}
+
 // InstallRequest is used for unmarshalling json objects directly into a request object
 // that the script factory can then digest and work with
 type InstallRequest struct{
-    Languages []string `json:"languages"`
-    Terminals []string `json:"terminals"`
-    Shells    []string `json:"shells"`
-    Browsers  []string `json:"browsers"`
-    Editors   []string `json:"editors"`
-    Tools     []string `json:"tools"`
-    Databases []string `json:"databases"`
+    Languages category `json:"Languages"`
+    Terminals category `json:"Terminals"`
+    Shells    category `json:"Shells"`
+    Browsers  category `json:"Browsers"`
+    Editors   category `json:"Editors"`
+    Tools     category `json:"Tools"`
+    Databases category `json:"Databases"`
 }
+
+
+func supportedMacPkgs() InstallRequest {
+    pkgCategories := []string{
+        "Languages",
+        "Terminals",
+        "Shells",
+        "Browsers",
+        "Editors",
+        "Tools",
+        "Databases",
+    }
+
+    pkgDesc := []string{
+        "Select all programming languages of your choice",
+        "Select all Terminal emulators of your choice",
+        "Select all terminal shells of your choice",
+        "Select all web browsers of your choice",
+        "Select all text editors of your choice",
+        "Select all developer tools of your choice",
+        "Select all databases of your choice",
+    }
+    
+    pkgItems := []map[string]string{
+        macos.SupportedLangs,
+        macos.SupportedTerminals,
+        macos.SupportedShells,
+        macos.SupportedBrowsers,
+        macos.SupportedEditors,
+        macos.SupportedTools,
+        macos.SupportedDatabases,
+    }
+
+    macPkgMap := map[string]category{}
+    
+    for index, currCategory := range pkgCategories {
+        var currentItems []string
+
+        for k := range pkgItems[index] {
+            currentItems = append(currentItems, k)
+        }
+
+        categoryStruct := category{
+            Description: pkgDesc[index],
+            Items: currentItems,
+        }
+
+        macPkgMap[currCategory] = categoryStruct 
+    }
+
+    macPkgs := InstallRequest{}
+    mapstructure.Decode(macPkgMap, &macPkgs)
+    return macPkgs
+}
+
+// MacPkgs is a list of All available packages
+var MacPkgs = supportedMacPkgs()
 
 
 // Helper function for inserting commands into the overall script slice.
@@ -50,14 +113,31 @@ func GenerateGeneric() (string, error) {
         return "", errors.New("The current file path isnt set")
     }
 
-    install := InstallRequest{
-        Languages: []string{"python", "ruby", "yolo", "java"},
-        Terminals: []string{"hyper", "fake-terminal", "iterm2", "yeet"},
-        Shells: []string{"zsh", "fish", "fsdfd"},
-        Browsers: []string{"google chrome", "opera",},
-        Editors: []string{"vim", "macvim", "sublime-text"},
+    genericPkgs := map[string]category{
+        "Languages":{
+            Description:"Select all programming languages of your choice",
+            Items: []string{"python", "ruby", "yolo", "java"},
+        },
+        "Terminals":{
+            Description: "testsjk",
+            Items: []string{},
+        },
+        "Shells":{
+            Description: "Select all Terminal shells of your choice",
+            Items: []string{"hyper", "fake-terminal", "itemr2", "yeet"},
+        },
+        "Browsers":{
+            Description: "Select the web browser of your choice",
+            Items: []string{"google chrome", "opera"},
+        },
+        "Editors":{
+            Description:"Select the text editor of your choice",
+            Items: []string{"vim", "macvim", "sublime-text"},
+        },
     }
 
+    var install = InstallRequest{} 
+    mapstructure.Decode(genericPkgs, &install)
 
     script := []string{}
 
@@ -68,11 +148,11 @@ func GenerateGeneric() (string, error) {
     // Setup script
     macos.InstallXCode(commander(&script))
     macos.InstallBrew(commander(&script))
-    macos.InstallLangs(commander(&script), install.Languages)
-    macos.InstallTerminals(commander(&script), install.Terminals)
-    macos.InstallShells(commander(&script), install.Shells)
-    macos.InstallBrowsers(commander(&script), install.Browsers)
-    macos.InstallEditors(commander(&script), install.Editors)
+    macos.InstallLangs(commander(&script), install.Languages.Items)
+    macos.InstallTerminals(commander(&script), install.Terminals.Items)
+    macos.InstallShells(commander(&script), install.Shells.Items)
+    macos.InstallBrowsers(commander(&script), install.Browsers.Items)
+    macos.InstallEditors(commander(&script), install.Editors.Items)
 
     // Generate a new uuid4
     uuid, err := uuid.NewRandom(); if err != nil {
